9b05ffc46ea3e7c46cbec76b757587772b38cddb
digimarkarts <digimarkarts@gmail.com>
2024-12-26 09:59:23 +0530

    Added for Newman

---
+++ postman-collection/test/unit/event.test.js
@@ -0,0 +1,194 @@
+var expect = require('chai').expect,
+    fixtures = require('../fixtures'),
+    sdk = require('../../lib/index.js'),
+
+    Event = sdk.Event,
+    Script = sdk.Script;
+
+describe('Event', function () {
+    var rawEvent = {
+        listen: 'test',
+        id: 'my-global-script-1',
+        script: {
+            type: 'text/javascript',
+            exec: 'console.log("hello");',
+            packages: {}
+        }
+    };
+
+    describe('sanity', function () {
+        describe('global event', function () {
+            var rawEvent = fixtures.collectionV2.event[0],
+                postmanEvent = new Event(rawEvent);
+
+            it('initializes successfully', function () {
+                expect(postmanEvent).to.be.ok;
+            });
+
+            describe('has property', function () {
+                it('listen', function () {
+                    expect(postmanEvent).to.have.property('listen', 'test');
+                });
+
+                it('script', function () {
+                    expect(postmanEvent).to.have.property('script');
+                });
+            });
+        });
+
+        describe('inline event', function () {
+            var rawEvent = fixtures.collectionV2.event[1],
+                postmanEvent = new Event(rawEvent);
+
+            it('initializes successfully', function () {
+                expect(postmanEvent).to.be.ok;
+            });
+
+            describe('has property', function () {
+                it('listen', function () {
+                    expect(postmanEvent).to.have.property('listen', 'prerequest');
+                });
+
+                it('script', function () {
+                    expect(postmanEvent).to.have.property('script').that.is.an('object');
+                    expect(postmanEvent.script).to.have.property('type', 'text/javascript');
+                    expect(postmanEvent.script).to.have.property('exec').that.is.an('array');
+                    expect(postmanEvent.script).to.have.property('packages').that.is.an('object');
+                });
+            });
+
+            it('should respect case for listen property', function () {
+                var postmanEvent = new Event({ id: 'dummy-id', listen: 'preRequest' });
+
+                expect(postmanEvent).to.have.property('listen', 'preRequest');
+            });
+        });
+    });
+
+    describe('json representation', function () {
+        it('must match what the event was initialized with', function () {
+            var event = new Event(rawEvent),
+                jsonified = event.toJSON();
+
+            expect(jsonified.listen).to.eql(event.listen);
+            // Script property checking is done independently
+            expect(jsonified).to.have.property('script');
+        });
+    });
+
+    describe('update', function () {
+        var script = 'console.log("This is a test log");';
+
+        it('must work with script instance definitions', function () {
+            var event = new Event(rawEvent);
+
+            event.update({ script: new Script({ exec: script }) });
+            expect(event.toJSON().script.exec).to.eql([script]);
+        });
+
+        it('must work with script strings', function () {
+            var event = new Event(rawEvent);
+
+            event.update({ script });
+            expect(event.toJSON().script.exec).to.eql([script]);
+        });
+
+        it('must work with script arrays', function () {
+            var event = new Event(rawEvent);
+
+            event.update({ script: [script] });
+            expect(event.toJSON().script.exec).to.eql([script]);
+        });
+
+        it('must correctly handle invalid/undefined script input', function () {
+            var event = new Event(rawEvent);
+
+            event.update();
+            expect(event.toJSON().script.exec).to.eql([rawEvent.script.exec]);
+        });
+
+        it('must correctly handle non-compliant script input', function () {
+            var event = new Event(rawEvent),
+                eventJSON;
+
+            event.update({});
+
+            eventJSON = event.toJSON();
+
+            expect(eventJSON).to.deep.include({
+                id: 'my-global-script-1',
+                listen: 'test'
+            });
+            expect(eventJSON).to.have.property('script').that.has.property('id');
+            expect(eventJSON.script).to.deep.include({
+                type: 'text/javascript',
+                exec: ['console.log("hello");'],
+                packages: {}
+            });
+        });
+
+        it('should update script id', function () {
+            var event = new Event(rawEvent),
+                beforeJSON = event.toJSON(),
+                afterJSON;
+
+            expect(beforeJSON).to.have.property('script').that.has.property('id');
+            expect(beforeJSON.script).to.deep.include({
+                type: 'text/javascript',
+                exec: ['console.log("hello");'],
+                packages: {}
+            });
+
+            event.update({ script: { id: 'my-new-script' } });
+            afterJSON = event.toJSON();
+
+            expect(beforeJSON.script.id).to.not.equal(afterJSON.script.id);
+            expect(afterJSON.script).to.have.property('id', 'my-new-script');
+            expect(afterJSON.script.exec).to.be.undefined;
+        });
+
+        it('should not add packages key if not present', function () {
+            var rawEvent = {
+                    listen: 'test',
+                    id: 'my-global-script-1',
+                    script: {
+                        type: 'text/javascript',
+                        exec: 'console.log("hello");'
+                    }
+                },
+                event = new Event(rawEvent),
+                beforeJSON = event.toJSON(),
+                afterJSON;
+
+            expect(beforeJSON.script).to.not.have.property('packages');
+
+            event.update({ script: { exec: 'console.log("updated");' } });
+            afterJSON = event.toJSON();
+
+            expect(afterJSON.script).to.not.have.property('packages');
+        });
+    });
+
+    describe('isEvent', function () {
+        var rawEvent = {
+            listen: 'test',
+            id: 'my-global-script-1',
+            script: {
+                type: 'text/javascript',
+                exec: 'console.log("hello");'
+            }
+        };
+
+        it('should return true for an Event instance', function () {
+            expect(Event.isEvent(new Event(rawEvent))).to.be.true;
+        });
+
+        it('should return false for a raw Event object', function () {
+            expect(Event.isEvent(rawEvent)).to.be.false;
+        });
+
+        it('should return false when called without arguments', function () {
+            expect(Event.isEvent()).to.be.false;
+        });
+    });
+});
